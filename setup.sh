c095c7abd29a22e91ff556f53c7ed8ff5f9f4c75
Sandy Suh <sandy@parsagon.io>
2022-01-31 23:11:10 +0000

    add mkcert

--- setup.sh
+++ setup.sh
@@ -1,8 +1,15 @@
+IP=$(dig @resolver4.opendns.com myip.opendns.com +short)
+mkdir ~/parsagon
+
 sudo apt update
 sudo apt -y upgrade
 
+sudo apt install libnss3-tools
+brew install mkcert
+mkcert -cert-file ~/parsagon/cert.pem -key-file ~/parsagon/key.pem $IP
+
 sudo apt -y install nginx
-sudo ufw allow 'Nginx Full'
+sudo ufw allow 'Nginx HTTPS'
 
 if [ ! -e /etc/nginx/sites-available/server.conf ]
 then
@@ -11,17 +18,6 @@ then
     sudo rm /etc/nginx/sites-enabled/default
 fi
 sudo systemctl restart nginx
-
-sudo snap install core
-sudo snap refresh core
-sudo snap install --classic certbot
-
-if [ ! -e /usr/bin/certbot ]
-then
-    sudo ln -s /snap/bin/certbot /usr/bin/certbot
-fi
-sudo certbot --nginx --config cert_cli.ini
-sudo ufw allow 'Nginx HTTPS'
 
 if ! command -v redis-server &> /dev/null
 then
