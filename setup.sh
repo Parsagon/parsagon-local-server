cf822cd959edf18063c3f1b36c70f353851550c5
Ubuntu <ubuntu@ip-172-31-23-9.us-east-2.compute.internal>
2022-01-31 23:39:46 +0000

    fix mkcert installation

--- setup.sh
+++ setup.sh
@@ -1,13 +1,19 @@
 IP=$(dig @resolver4.opendns.com myip.opendns.com +short)
-mkdir ~/parsagon
+mkdir -p ~/parsagon
+cd ~/parsagon
 
 sudo apt update
 sudo apt -y upgrade
 
-sudo apt install libnss3-tools
-git clone https://github.com/FiloSottile/mkcert && cd mkcert
-go build -ldflags "-X main.Version=$(git describe --tags)"
-cd -
+if [ ! -e /usr/local/bin/mkcert ]
+then
+    sudo apt -y install libnss3-tools
+    sudo apt -y install golang-go
+    git clone https://github.com/FiloSottile/mkcert && cd mkcert
+    go build -ldflags "-X main.Version=$(git describe --tags)"
+    sudo cp mkcert /usr/local/bin/mkcert
+    sudo chmod +x /usr/local/bin/mkcert
+fi
 mkcert -cert-file ~/parsagon/cert.pem -key-file ~/parsagon/key.pem $IP
 
 sudo apt -y install nginx
@@ -20,17 +26,18 @@ then
     sudo rm /etc/nginx/sites-enabled/default
 fi
 sudo systemctl restart nginx
+cd -
 
-if ! command -v redis-server &> /dev/null
-then
-    sudo apt -y install make
-    sudo apt -y install tcl
-    cd
-    wget http://download.redis.io/redis-stable.tar.gz
-    tar xvzf redis-stable.tar.gz
-    cd redis-stable
-    make
-    sudo make install
-    sudo sh -c 'echo "vm.overcommit_memory = 1" >> /etc/sysctl.conf'
-    sudo sysctl vm.overcommit_memory=1
-fi
+#if ! command -v redis-server &> /dev/null
+#then
+    #sudo apt -y install make
+    #sudo apt -y install tcl
+    #cd
+    #wget http://download.redis.io/redis-stable.tar.gz
+    #tar xvzf redis-stable.tar.gz
+    #cd redis-stable
+    #make
+    #sudo make install
+    #sudo sh -c 'echo "vm.overcommit_memory = 1" >> /etc/sysctl.conf'
+    #sudo sysctl vm.overcommit_memory=1
+#fi
