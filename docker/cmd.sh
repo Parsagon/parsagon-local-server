7a2ed9d9def8f6c1da7b726b2a494060a3b1ccff
Gabe Montague <montague.gabe@gmail.com>
2022-04-12 18:08:40 -0400

    Fix Chrome issues for selenium

--- docker/cmd.sh
+++ docker/cmd.sh
@@ -7,4 +7,11 @@ service supervisor start
 supervisorctl start all
 
 # To keep the container running:
-bash
+tail -f /var/log/redis/redis-server.log & \
+tail -f /var/log/nginx/access.log & \
+tail -f /var/log/nginx/error.log & \
+tail -f /var/log/supervisor/celery-stderr.log & \
+tail -f /var/log/supervisor/celery-stdout.log & \
+tail -f /var/log/supervisor/daphne-stderr.log & \
+tail -f /var/log/supervisor/daphne-stdout.log & \
+tail -f /var/log/supervisor/supervisord.log 
--- docker/run.sh
+++ docker/run.sh
@@ -1,30 +1,26 @@
 #!/usr/bin/env bash
 
-# Example: ./docker/run.sh gabe.parsagon.io 1fc672c23791018556759fdfc5ab852606d99dff
+# Example: ./docker/run.sh 1fc672c23791018556759fdfc5ab852606d99dff
 
 fail_usage () {
-	printf "Usage: ./docker/run.sh PARSAGON_HOST API_KEY\n"
+	printf "Usage: ./docker/run.sh API_KEY\n"
 	exit 1
 }
+
+PARSAGON_HOST="host.docker.internal:8000"
 
 if [ -z "$1" ]; then
 	fail_usage
 fi
-
-if [ -z "$2" ]; then
-	fail_usage
-fi
-
-PARSAGON_HOST=$1
-shift
-
-API_KEY=$2
+API_KEY=$1
 shift
 
 set -x
 docker run -it --rm \
 	-p 8080:80 \
-	-e "PARSAGON_HOST=$PARSAGON_HOST" \
 	-e "API_KEY=$API_KEY" \
+	-e "PARSAGON_HOST=$PARSAGON_HOST" \
+	--security-opt seccomp=./docker/chrome.json \
+	--add-host=host.docker.internal:host-gateway \
 	pslocal:latest "$@"
 set +x
