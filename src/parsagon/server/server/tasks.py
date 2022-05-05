d147e3c227218cae230f698881655b0104de7cf4
Sandy Suh <sandy@parsagon.io>
2022-05-05 22:10:38 +0000

    add build_structure to tasks

--- src/parsagon/server/server/tasks.py
+++ src/parsagon/server/server/tasks.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 from celery import shared_task
 from django.conf import settings
+from server.utils import build_structure
 
 import requests
 import datetime
